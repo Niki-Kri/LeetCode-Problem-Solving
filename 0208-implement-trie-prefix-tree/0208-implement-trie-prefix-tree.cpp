53ef699efd6f88594b7a78a78d37fed8757296ea
Niki Kumari <2024ugcs073@gmail.com>
2026-09-02 19:47:50 +0530

    Time: 18 ms (70.94%), Space: 50.5 MB (53.96%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,51 @@
+class TrieNode {
+public:
+    TrieNode* children[26] = {};
+    bool isEndOfWord = false;
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int index = c - 'a';
+            if (!node->children[index]) {
+                node->children[index] = new TrieNode();
+            }
+            node = node->children[index];
+        }
+        node->isEndOfWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char c : word) {
+            int index = c - 'a';
+            if (!node->children[index]) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return node->isEndOfWord;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char c : prefix) {
+            int index = c - 'a';
+            if (!node->children[index]) {
+                return false;
+            }
+            node = node->children[index];
+        }
+        return true;
+    }
+};
